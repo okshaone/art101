4a305cb5f640eee4c016a30bdb3d900fbc184dad
okshaone <okshaone@gmail.com>
2022-04-27 20:34:37 -0700

    :D

---
+++ lab6/js/lab.js
@@ -0,0 +1,20 @@
+/*
+ *Author: Zibo Chen, Stephen Wei
+ *Create: 25 April, 2022
+ *License: Public Domain
+ */
+ myTransport = ["Car", "Bus", "Bike"];
+
+var myMainRide = {
+  make: 'Maserati',
+  model: 'Ghibli',
+  color: 'White',
+  year: 2016,
+  age: function(){
+    return 2022 - age;
+ }
+};
+
+document.writeln("Kinds of transportion I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
