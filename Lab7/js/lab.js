9e4e2f96f38dff94d5661abf05f0d2c0b8dada09
okshaone <okshaone@gmail.com>
2022-04-28 01:26:18 -0700

    123

--- Lab7/js/lab.js
+++ Lab7/js/lab.js
@@ -5,23 +5,23 @@
  */
 
  function sortUserName() {
-   var userName = window.prompt("Hi, my name is the great WEdsde, your name sucks so please let me help you get a new one, enter your current name here.");
+   //to take user input
+   var userName = window.prompt("Hi, my name is the GREAT SDFEWWE, your name sucks so please let me help you get a new one, enter your current name here.");
    console.log("userName =", userName);
-
+   //split string to array
    var nameArray = userName.split('');
    console.log("nameArray =", nameArray);
-
+   //sort the array
    var nameArraySort = nameArray.sort();
    console.log("nameArraySort =", nameArraySort);
-
+   //join array back to string
    var nameSorted = nameArraySort.join('');
-   console.log("nameSorted =", nameSorted);
-
+   console.log("nameSorted =",nameSorted);
    var nameToUpperCase = nameSorted.toUpperCase('');
    console.log("nameToUpperCase =",nameToUpperCase);
-
+   //this convert all the strings to uppercase
    return nameToUpperCase;
  }
-
- document.writeln("You can now proudly call yourself the great ",
+ //output
+ document.writeln("You can now proudly call yourself the GREAT ",
    sortUserName(), "</br>");
