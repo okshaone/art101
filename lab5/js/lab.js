7189e765aa5b65c2ad15f5b6c308260323afc311
okshaone <okshaone@gmail.com>
2022-04-27 20:31:52 -0700

    ：D

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+
+/*
+ * Author:    Zibo Chen, Stephen Wei
+ * Created:   04.20.2022
+ * License: Public Domain
+ */
+
+//Define variables
+make = "Maserati";
+model = "Ghibli";
+color = "White";
+year = "2016";
+var ownIt = true;
+
+//Calculate
+age = 2022 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
